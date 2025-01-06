e0b01ea990fbc362d0503bbbb01a4c88a0d45427
wishalta <rolandas.lebed@gmail.com>
2025-01-06 22:27:37 +0200

    Everything in one file(fix) Working through jupyter file

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from pathlib import Path
-from File_open_close import file_open_close
+# from File_open_close import file_open_close
 import pandas as pd
 
-# FILES_PATH = Path("2_logistic_regression_files/")
-# FILES_PATH.mkdir(parents=True, exist_ok=True)
+FILES_PATH = Path("2_logistic_regression_files/")
+FILES_PATH.mkdir(parents=True, exist_ok=True)
 
-raw_df = pd.read_csv("weatherAUS.csv")
-tear = file_open_close(raw_df)
-print(tear)
+def file_open():
+    raw_df = pd.read_csv("weatherAUS.csv")
+    return raw_df
 
-read = tear.file_interact()
-file = pd.DataFrame(read)
-print(file)
+'''We are trying to predict about tomorrow, will it rain or not?'''
 
+# print(file_open())
+# file_open().info(max_cols=len(file_open()))
+
+test1 = file_open()                                            #<----Copy to not lose first data
+test1.dropna(subset=['RainToday', 'RainTomorrow'], inplace=True) #<----#Good idea to discard the rows where the value of RainTomorrow or
+                                                                       #RainToday is missing to make our analysis and modeling simpler (since one of them is the target
+                                                                       #variable, and the other is likely to be very closely related to the target variable).
+# file_open().info(max_cols=len(file_open())) /Comparing data\
+# test1.info(max_cols=len(test1))             \Comparing data/
